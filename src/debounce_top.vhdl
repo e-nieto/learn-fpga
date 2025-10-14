038710a56b223ef2d197d0371eeb2cde30c8e1fd
Edgar Nieto <edgarnietomartin@gmail.com>
2025-10-15 00:41:46 +0200

    Include Debounce
    Includes the debounce entity and top architecture entity. It compiles up
    to bitstream.

--- src/debounce_top.vhdl
+++ src/debounce_top.vhdl
@@ -4,17 +4,37 @@ use ieee.std_logic_1164.all;
 entity debounce_top is
 	port
 	(
-		i_Clk;      in  std_logic
-		i_Switch_1: in  std_logic,
+		i_Clk:      in  std_logic;
+		i_Switch: in  std_logic;
 		o_LED:      out std_logic
 	);
+
 end entity debounce_top;
 
+
 architecture RTL of debounce_top is
-	signal w_Debounce_Switch: std_logic;
+	signal w_Debounced_Switch: std_logic;
+
 begin
 	Debounce_Inst: entity work.Debounce_Filter
 		generic map
 		(
-			
+			DEBOUNCE_LIMIT => 250000
 		)
+
+		port map
+		(
+			i_Clk => i_Clk,
+			i_Bouncy => i_Switch,
+			o_Debounced => w_Debounced_Switch
+		);
+
+	LED_Toggle_Inst: entity work.led_toggle
+		port map
+		(
+			i_Clk => i_Clk,
+			i_Switch => w_Debounced_Switch,
+			o_LED => o_LED
+		);
+
+end architecture RTL;
